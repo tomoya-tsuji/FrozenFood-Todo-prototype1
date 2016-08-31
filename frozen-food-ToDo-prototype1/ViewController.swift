e574760dc8deccdb0ef89003250a49beb2e55359
tomoya-tsuji <1020tomoya@gmail.com>
2016-09-01 02:29:56 +0900

    Initial Commit

---
+++ frozen-food-ToDo-prototype1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  frozen-food-ToDo-prototype1
+//
+//  Created by 辻朝也 on 2016/09/01.
+//  Copyright © 2016年 astrotom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
